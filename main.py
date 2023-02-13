31f6f6dc36dc5e663cbda2549f8f1e1a80b270b1
Toby <kommandertoby@gmail.com>
2023-02-13 07:53:48 +0000

    GUI for program

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-# Import Libs
 import time
 import ctypes
 import winsound
+import tkinter as tk
 
-# Stopwatch
 def stopwatch():
     start_time = time.time()
     while True:
         time.sleep(0.01)
         elapsed_time = time.time() - start_time
-        print("\rElapsed time: %.2f seconds" % elapsed_time, end="")
+        label["text"] = "Elapsed time: %.2f seconds" % elapsed_time
+        root.update()
         if ctypes.windll.user32.GetAsyncKeyState(0x0D) != 0: # 0x0D is the virtual key code for the Enter key
-            print("\nStopwatch reset.")
+            label["text"] = "Stopwatch reset."
             start_time = time.time()
-        # Play sound after 60 seconds
         if int(elapsed_time) % 60 == 0:
             winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
 
+root = tk.Tk()
+root.title("Stopwatch")
+label = tk.Label(root, text="")
+label.pack(pady=20)
+
 if __name__ == "__main__":
     stopwatch()
+    root.mainloop()
+
+# is used to hide the root window. The root window is not needed for the stopwatch to work, so it is hidden.
+root.withdraw()
